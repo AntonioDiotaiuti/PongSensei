3db3eb0124654ff4b2dab2c0136e1bd1a14c3ca2
Salkazko <daloludo@gmail.com>
2025-05-01 15:49:09 +0200

    DEMOShooting DONE

---
+++ Assets/Scenes/DEMOShooting/DEMOscript/Projectile.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10f;
+    public float lifetime = 2f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+}
