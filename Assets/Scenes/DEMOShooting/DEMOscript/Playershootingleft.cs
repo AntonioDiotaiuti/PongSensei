98220ddf573b6175b1a28ad7fe9fbe8618708c72
christian bugiani <christianbugiani2005@gmail..com>
2025-05-06 18:06:47 +0200

    aggiunto il "FinalPlayerShoting" il modo di sparare definitivo mk + pad

--- Assets/Scenes/DEMOShooting/DEMOscript/Playershootingleft.cs
+++ Assets/Scenes/DEMOShooting/DEMOscript/Playershootingleft.cs
@@ -1,19 +1,32 @@
 using UnityEngine;
 
-using UnityEngine;
-
 public class PlayerShootingleft : MonoBehaviour, IPlayerShooter
 {
     public GameObject projectilePrefab;
     public Transform firePoint;
-    [SerializeField]public float projectileSpeed = 10f;
+    [SerializeField] public float projectileSpeed = 10f;
     private bool hasAmmo = true;
+    private bool triggerHeld = false;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.L) && hasAmmo)
+        // Tasto tastiera
+        bool keyboardShoot = Input.GetKeyDown(KeyCode.L);
+
+        // L2 controller (3rd axis)
+        float l2Value = Input.GetAxis("FireL2");
+        bool controllerShoot = l2Value > 0.5f && !triggerHeld;
+
+        if ((keyboardShoot || controllerShoot) && hasAmmo)
         {
             Shoot();
+            triggerHeld = true; // previene spam mentre L2 è tenuto premuto
+        }
+
+        // reset quando L2 rilasciato
+        if (l2Value < 0.3f)
+        {
+            triggerHeld = false;
         }
     }
 
@@ -28,8 +41,9 @@ public class PlayerShootingleft : MonoBehaviour, IPlayerShooter
         }
         hasAmmo = false;
     }
+
     public void Reload()
     {
-            hasAmmo = true;
+        hasAmmo = true;
     }
 }
