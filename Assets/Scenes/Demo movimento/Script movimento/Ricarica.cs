760c79ae7cc0153b21769314878e95bf9b44c795
christian bugiani <christianbugiani2005@gmail..com>
2025-05-20 13:05:27 +0200

    ecco a te Cosimone

--- Assets/Scenes/Demo movimento/Script movimento/Ricarica.cs
+++ Assets/Scenes/Demo movimento/Script movimento/Ricarica.cs
@@ -6,6 +6,9 @@ public class Bullet : MonoBehaviour
     public float speed = 20f;
     public Vector2 direction = Vector2.right;
 
+    // morte
+    public bool Death = false;
+
     void Update()
     {
         transform.Translate(direction * speed * Time.deltaTime);
@@ -15,10 +18,12 @@ public class Bullet : MonoBehaviour
     {
         if (collision.CompareTag("Player1"))
         {
+            Death = true; // attiva booleana
             StartCoroutine(HandlePlayerDeath(collision.gameObject, "Player2"));
         }
         else if (collision.CompareTag("Player2"))
         {
+            Death = true;
             StartCoroutine(HandlePlayerDeath(collision.gameObject, "Player1"));
         }
     }
@@ -33,34 +38,12 @@ public class Bullet : MonoBehaviour
             yield break;
         }
 
-        //Ferma il proiettile
+        // Ferma il proiettile
         gameObject.SetActive(false);
 
         // Trigger animazione Hurt
         anim.SetTrigger("isHurt");
-        /*
-        //Aspetta che HurtP1 sia attiva
-        while (!anim.GetCurrentAnimatorStateInfo(0).IsName("HurtP1"))
-            yield return null;
 
-        //Aspetta la durata reale di HurtP1
-        yield return new WaitForSecondsRealtime(anim.GetCurrentAnimatorStateInfo(0).length);
-
-        //Trigger animazione Death
-        //anim.SetTrigger("isDeath");
-
-        //Aspetta che DeathP1 sia attiva
-        while (!anim.GetCurrentAnimatorStateInfo(0).IsName("DeathP1"))
-            yield return null;
-
-        //Aspetta la durata reale di DeathP1
-        yield return new WaitForSecondsRealtime(anim.GetCurrentAnimatorStateInfo(0).length);
-
-        // Distruggi player
-        Destroy(player);
-
-        //Vittoria + blocco gioco
-        GameManager.Instance.DeclareVictory(winner);
-        */
+        
     }
 }
