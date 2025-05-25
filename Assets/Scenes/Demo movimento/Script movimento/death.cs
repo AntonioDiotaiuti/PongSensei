96c557bfb9d357f4e5e2fd5ac439d6bffe750550
christian bugiani <christianbugiani2005@gmail..com>
2025-05-25 18:02:36 +0200

    aggiunto audio morte, hurt, sparo,riscarica(da fixare) musica di sottofondo e vittoria

--- Assets/Scenes/Demo movimento/Script movimento/death.cs
+++ Assets/Scenes/Demo movimento/Script movimento/death.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 public class death : MonoBehaviour
 {
-    
+    public AudioClip hurtSound; // assegna il suono da Inspector
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            // Se non c'è un AudioSource, lo aggiungiamo dinamicamente
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
     public void ReadDead1(bool isDead1)
     {
-        if (isDead1 != false)
+        if (isDead1)
         {
             //StartCoroutine(HandlePlayerDeath("Player1", "Player2"));
         }
@@ -14,13 +26,12 @@ public class death : MonoBehaviour
 
     public void ReadDead2(bool isDead2)
     {
-        if (isDead2 != false)
+        if (isDead2)
         {
             //StartCoroutine(HandlePlayerDeath("Player2", "Player1"));
         }
     }
 
-   
     public void HandlePlayerDeath()
     {
         Animator anim = GetComponent<Animator>();
@@ -33,12 +44,18 @@ public class death : MonoBehaviour
             {
                 winnerPlayer = "2";
             }
-            GameManager.Instance.DeclareVictory("Player "+ winnerPlayer);
-        } else
+            GameManager.Instance.DeclareVictory("Player " + winnerPlayer);
+        }
+        else
         {
             // Trigger animazione Hurt
             anim.SetTrigger("isHurt");
+
+            // Riproduci il suono
+            if (hurtSound != null && audioSource != null)
+            {
+                audioSource.PlayOneShot(hurtSound);
+            }
         }
     }
 }
-
